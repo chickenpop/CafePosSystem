c85b1ccb6a9d5c829d31af08d7778e55b351b0eb
chickenpop <1005honeybadger@gmail.com>
2022-05-29 21:16:22 +0900

    월, 연매출 금액 출력 기능

--- src/cafepossystem/management/menu/CafeRevenueCRUD.java
+++ src/cafepossystem/management/menu/CafeRevenueCRUD.java
@@ -8,22 +8,56 @@ import cafepossystem.data.OrderHistoryList;
 public class CafeRevenueCRUD {
 
 	private Calendar now;
+	private int totalPrice;
 	
 	public CafeRevenueCRUD() {
 		this.now = Calendar.getInstance();
+		this.totalPrice = 0;
 	}
 
 	public void readAnnualSales() {
+		this.totalPrice = 0;
+		Output.bar();
+		Output.subTitle("연매출");
+		Output.bar();
+		for(OrderHistoryList ohl : Data.orderHistoryList) {
+			if(String.format("%04d", now.get(Calendar.YEAR)).equals(ohl.getOrderNum().substring(0, 4))) {
+				totalPrice += ohl.getPrice();
+			}
+		}
 		
+		if(totalPrice == 0) {
+			System.out.println("현재 판매된 내역이 조회되지 않습니다.");
+		} else {
+			System.out.printf("연매출 : %d원\n", totalPrice);			
+		}
+		Output.bar();
+		Output.Waiting();
 	}
 	
 	public void readMonthSales() {
+		this.totalPrice = 0;
+		Output.bar();
+		Output.subTitle("월매출");
+		Output.bar();
+		for(OrderHistoryList ohl : Data.orderHistoryList) {
+			if(String.format("%02d", now.get(Calendar.MONTH)+1).equals(ohl.getOrderNum().substring(5, 7))) {
+				totalPrice += ohl.getPrice();
+			}
+		}
 		
+		if(totalPrice == 0) {
+			System.out.println("현재 판매된 내역이 조회되지 않습니다.");
+		} else {
+			System.out.printf("월매출 : %d원\n", totalPrice);			
+		}
+		Output.bar();
+		Output.Waiting();
 	}
 
 	public void readDaySales() {
 		
-		int totalPrice = 0;
+		this.totalPrice = 0;
 		Output.bar();
 		Output.subTitle("일매출");
 		Output.bar();
